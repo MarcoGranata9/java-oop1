b8942604f79ffe02696041bd4c1b591995c52e17
MarcoGranata9 <granatam9900@gmail.com>
2024-05-17 15:05:33 +0200

    finished javashop

--- src/org/learning/javashop/Main.java
+++ src/org/learning/javashop/Main.java
@@ -1,2 +1,23 @@
-package org.learning.javashop;public class Main {
+package org.learning.javashop;
+
+public class Main {
+    public static void main(String[] args) {
+        Prodotto monitor = new Prodotto("samsung", "schermo", 200);
+
+
+        monitor.setName("LG");
+        monitor.setDesc("monitor");
+        monitor.setPrice(100);
+        monitor.setIva(25);
+
+        System.out.println(monitor.getCode());
+        System.out.println(monitor.getName());
+        System.out.println(monitor.getDesc());
+        System.out.println(monitor.getPrice());
+        System.out.println(monitor.getIva());
+
+
+        System.out.println(monitor.getFullPrice());
+        System.out.println(monitor.getFullName());
+    }
 }
--- src/org/learning/javashop/Prodotto.java
+++ src/org/learning/javashop/Prodotto.java
@@ -1,2 +1,70 @@
-package org.learning.javashop;public class prodotto {
+package org.learning.javashop;
+
+import java.util.Random;
+
+public class Prodotto {
+    // Argomenti
+    private int code;
+    private String name;
+    private String desc;
+    private double price;
+    private int iva;
+    Random rnd = new Random();
+
+    // Costruttore
+    Prodotto(String name,String desc, double price){
+        code = rnd.nextInt(999999);
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        iva = 22;
+    }
+
+    // Metodi
+
+    public double getFullPrice() {
+        return price + ((price / 100) * iva);
+    }
+
+    public String getFullName() {
+        return getCode() + "-" + name;
+    }
+
+    // Getters
+    public String getCode() {
+        return String.format("%06d", code);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+    // Setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
 }
